bbbc3fe32eafae1c7d2e9b53c05b9b02a3e11074
andy-box31 <andy@box31.com>
2019-06-05 22:46:16 +0100

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send("Nothing to see here :("));
+app.get('/', (req, res) => res.send("Nothing to see here :( maybe it will work?"));
 app.get('/healthcheck', (req, res) => res.send("I am healthy!"));
 
 // app.get('/getView', (req, res) => res.json(myView));
