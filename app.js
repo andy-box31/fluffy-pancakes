b8dba95b8d2ea95edb90a2a2a71f7a3a880fe96c
andy-box31 <andy@box31.com>
2019-06-06 21:49:51 +0100

    express

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 3000
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.send("Nothing to see here :("));
 app.get('/healthcheck', (req, res) => res.send("I am healthy!"));
 
-// app.get('/getView', (req, res) => res.json(myView));
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
