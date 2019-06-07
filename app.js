122b26869be5fc231fe6ca7883f184a1ea4b80f1
andy-box31 <andy@box31.com>
2019-06-07 23:16:45 +0100

    added static data enedpoint, tested from FE

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require('express')
+const path = require('path')
+const transformers = require('./data/transformers.json')
+const port = process.env.PORT || 3000
+
 const app = express();
 
-const port = process.env.PORT || 3000
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send("Nothing to see here :("));
+// app.get('/', (req, res) => res.send("Nothing to see here :("));
 app.get('/healthcheck', (req, res) => res.send("I am healthy!"));
+
+app.get('/data/transformers', (req, res) =>res.send(transformers))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
