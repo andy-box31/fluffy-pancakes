07e79c99859b7f15ab2812c2116bdef32ff45546
andy-box31 <andy@box31.com>
2019-06-25 12:34:42 +0100

    added basic card component

--- client/components/App/App.jsx
+++ client/components/App/App.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import Api from '../../services/api'
 
+import Card from '../Card/Card'
+
 class App extends React.Component {
   constructor (props) {
     super (props)
 
     this.getDatas()
+    this.state= {transformers: []}
   }
 
   async getDatas() {
     const transformers = await Api.get('transformers')
-    console.log(transformers)
+    this.setState({transformers: transformers})
   }
   render () {
-    return <h1>Yo, {this.props.name}</h1>
+    const cardList = this.state.transformers.map((transformer) => <Card params={transformer} />)
+
+    return (
+    <React.Fragment>
+      <h1>Yo, {this.props.name}</h1>
+      {this.state.transformers.length > 0 && cardList}
+    </React.Fragment>
+    )
   }
 }
 
---
+++ client/components/Card/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Card ({params}) {
+  console.log(params)
+  const details = Object.keys(params).map((key, i) => 
+    <li>{key}: {params[key]}</li>
+  )
+
+  return (
+    <div>
+      {params.Name ? params.Name : 'Card info'}
+      <ul>{details}</ul>
+    </div>
+  )
+}
+
+export default Card
