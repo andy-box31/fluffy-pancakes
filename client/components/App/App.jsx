45c6edf96b960c5d662c193cf3d9eec9eb981bb8
andy-box31 <andy@box31.com>
2019-06-25 11:59:41 +0100

    added axios

--- client/components/App/App.jsx
+++ client/components/App/App.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import Api from '../../services/api'
 
 class App extends React.Component {
+  constructor (props) {
+    super (props)
+
+    this.getDatas()
+  }
+
+  async getDatas() {
+    const transformers = await Api.get('transformers')
+    console.log(transformers)
+  }
   render () {
     return <h1>Yo, {this.props.name}</h1>
   }
