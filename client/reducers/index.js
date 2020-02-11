630988b05c33fb4b94fe21ee016cb00271dbfb7a
Andy Phillips <andy-box31@users.noreply.github.com>
2020-02-11 15:57:18 +0000

    Update client/reducers/index.js
    undo accidental change

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -4,7 +4,7 @@ import shuffle from '../utilities/shuffle'
 const initialState = {
   cards: [],
   deckInfo: {competeOn: []},
-  playmode: PLAY_MODE.VS_LOCAL,
+  playmode: PLAY_MODE.VS_COPMUTER,
   activePlayer: null,
   winner: null,
   gameState: GAME_STATE.PRE_GAME,
