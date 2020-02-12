0684d90581819e613c53db988a40b52151404cd7
andy-box31 <andy@box31.com>
2020-02-12 22:20:33 +0000

    const

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
 const transformers = require('../data/transformers')
 const dinosaurs = require('../data/dinosaurs.json')
 const short = require('../data/transformersShort.json')
