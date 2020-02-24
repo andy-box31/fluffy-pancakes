966df0e59eb80ec0d67ff8745a872920da713d57
andy-box31 <andy@box31.com>
2020-02-24 13:29:08 +0000

    thing

--- client/utilities/debounce.js
+++ client/utilities/debounce.js
@@ -2,7 +2,8 @@
 export default function debounce (func, wait, immediate) {
   var timeout
   return function () {
-    var context = this; var args = arguments
+    var context = this
+    var args = arguments
     var later = function () {
       timeout = null
       if (!immediate) func.apply(context, args)
